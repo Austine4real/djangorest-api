9ed96529b08e6b691f90b19560f8ceecc10df06f
Ugbodaga Augustine <83676555+Austine4real@users.noreply.github.com>
2023-01-24 06:30:31 +0100

    update

--- api/settings.py
+++ api/settings.py
@@ -23,10 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--2mpjpwflz0m2t4&+iun8*bq7*wu#d!(l-1k6p*35ks#&7^tv5'
 
 # SECURITY WARNING: don't run with debug turned on in production!py
-DEBUG = False
+# DEBUG = False
+
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['localhost','django-ubos.herokuapp.com']
+# ALLOWED_HOSTS = ['localhost','django-ubos.herokuapp.com']
+
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
